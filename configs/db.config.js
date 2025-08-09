d3df7b6087d106bf6d15bc4180d42fd2ee50447c
mwahyudihd <muh.wahyudi282828@gmial.com>
2025-08-09 16:06:49 +0700

    minor: reconfig database

--- configs/db.config.js
+++ configs/db.config.js
@@ -7,16 +7,31 @@ let sequelize;
 // Cek apakah aplikasi berjalan di lingkungan production (seperti Railway)
 // Jika ya, gunakan DATABASE_URL yang disediakan Railway
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
+  // sequelize = new Sequelize(process.env.DATABASE_URL, {
+  //   dialect: 'postgres',
+  //   protocol: 'postgres',
+  //   // Opsi ini penting untuk koneksi ke database di layanan cloud seperti Railway
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false // Diperlukan untuk koneksi ke database Railway
+  //     }
+  //   },
+  //   logging: false,
+  // });
+
+  const dbConfig = {
+    host: process.env.DB_HOST_PRODUCTION,
+    port: process.env.DB_PORT,
+    username: process.env.DB_HOST_USER,
+    password: process.env.DB_HOST_PASSWORD,
+    database: process.env.DB_HOST_NAME,
+  }
+
+  sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+    host: dbConfig.host,
+    port: dbConfig.port,
     dialect: 'postgres',
-    protocol: 'postgres',
-    // Opsi ini penting untuk koneksi ke database di layanan cloud seperti Railway
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // Diperlukan untuk koneksi ke database Railway
-      }
-    },
     logging: false,
   });
 } else {
